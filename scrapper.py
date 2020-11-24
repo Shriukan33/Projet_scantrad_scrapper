9dc86440e71e38a39388708b96d747956a3a99bc
Shriukan33 <benja.mourgues@hotmail.fr>
2020-11-24 16:11:44 +0100

    Updated - Fetch the title, last release date and link.

--- scrapper.py
+++ scrapper.py
@@ -1,39 +1,65 @@
 # -*- coding: utf-8 -*-
+"""
+This scrapper is used to get basic information on releases on the website
+scantrad-union.com and save them in tracking.csv
+"""
 
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+from os import path
 
-def get_last_chapter(link):
-    """
-    Gets the last chapter of a given manga with the url of the main page 
-    of the website scantrad-union.com
-
-    Parameters
-    ----------
-    link : str
-        link must be an URL directing to the main page of the manga.
-        Exemple : https://scantrad-union.com/manga/solo-leveling/
-
-    Returns
-    -------
-    chapter : string
-        The last available chapter. 
-
-    """
-    # Gets the html code and search for the last chapter out
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, "html.parser")
-    chapter = soup.find_all("span", class_ = "chapter-number")[0]
-    chapter = str(chapter)
+def html_parser(link):
     
-    # Deletes the html tags to get only the number of the chapter
-    skips = ['<span class="chapter-number">#',"</span>"]
-    for char in skips:
-        chapter = chapter.replace(char, "")
+    try:
+        page = requests.get(link)
+        soup = BeautifulSoup(page.content, "html.parser")
+    except:
+        RuntimeError(link + " might be down or unreachable")
+        quit()
         
-    return chapter
+    return page, soup
 
+def get_info(link):
+    """ 
+    Returns basic informations about a manga's mainpage as strings
+    """
+    page, soup = html_parser(link)
+    
+    chapter = soup.find_all("span", class_ = "chapter-number")[0]\
+        .get_text().replace("#", "")
+    title = soup.select("div h2")[0].get_text()
+    last_release = soup.find_all("span", style="margin: 0 15px;float: right;")[0].get_text()
+    
+    return chapter, title, last_release, link
 
-url = "https://scantrad-union.com/manga/star-martial-god-technique/"
-chapter = get_last_chapter(url)
+# List of mangas I track
+tracked = ["https://scantrad-union.com/manga/solo-leveling/",
+           "https://scantrad-union.com/manga/volcanic-age/"]
 
+# Getting basic informations to track
+for link in tracked:
+    chapter , title, last_release, link = get_info(link)
+
+    # Inserting the previously collected datas in database
+    data = [{
+        "Name": title,
+        "Last chapter": chapter,
+        "Last release": last_release,
+        "Link": link}]
+    
+    # Creates or update the database
+    if path.exists("tracking.csv"):
+        tracking_database = pd.read_csv("tracking.csv")
+        tracking_database = tracking_database.append(data)
+        tracking_database.to_csv("tracking.csv", index=False)
+    else:
+        tracking_database = pd.DataFrame(data)
+        tracking_database.to_csv("tracking.csv", index=False)
+        
+# Deleting the duplicates
+tracking_database = pd.read_csv("tracking.csv")
+tracking_database.drop_duplicates(subset=["Name"] , keep='last', inplace=True)
+tracking_database.to_csv("tracking.csv", index=False)
+
+print(tracking_database)
